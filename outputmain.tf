a1563975c2319ce1e1c2dffc590577a10ade38c8
Kiran <kiranhiremath2192@gmail.com>
2026-02-13 23:55:13 +0530

    two files

---
+++ outputmain.tf
@@ -0,0 +1,21 @@
+
+provider "aws" {
+  region = "ap-south-1"
+}
+data "aws_ami" "ubuntu" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
+output "aws_ami" {
+  value = data.aws_ami.ubuntu.id
+}
+
+  
+
+
